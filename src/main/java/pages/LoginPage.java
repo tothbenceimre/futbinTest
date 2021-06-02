f0feaa8d1c423786c4bebed359a61a9d67788f22
tothbenceimre <benceimre.toth@gmail.com>
2021-06-02 16:50:31 +0200

    added username and password fields and methods for it

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import util.UtilDriver;
+import util.Waiter;
+
+public class LoginPage {
+    WebDriver driver;
+    Waiter waiter;
+
+    @FindBy (id = "Email") WebElement username;
+    @FindBy (id = "Password") WebElement password;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        this.waiter = new Waiter(this.driver);
+        PageFactory.initElements(this.driver, this);
+    }
+
+    public void fillUsernameField (String username) {
+        this.username.sendKeys(username);
+    }
+
+    public void fillPasswordField (String password) {
+        this.password.sendKeys(password);
+    }
+
+    public void logIn (String username, String password) {
+        fillUsernameField(username);
+        fillPasswordField(password);
+    }
+}
