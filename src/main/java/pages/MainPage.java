b461fac6d67a5f34c78eb39d275682a6ee8a9f50
tothbenceimre <benceimre.toth@gmail.com>
2021-06-02 16:42:02 +0200

    added loginButton and methods for it

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -10,9 +10,15 @@ public class MainPage {
     WebDriver driver;
     Waiter waiter;
 
+    @FindBy(xpath = "//a[@class='nav-link waves-effect waves-light'][normalize-space()='Login']") WebElement logInButton;
+
     public MainPage (WebDriver driver) {
         this.driver = driver;
         this.waiter = new Waiter(this.driver);
         PageFactory.initElements(this.driver, this);
+    }
+
+    public void clickOnLoginButton () {
+        this.logInButton.click();
     }
 }
